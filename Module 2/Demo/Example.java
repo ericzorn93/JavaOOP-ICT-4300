6b0df9c64c0db3fe8fb2f5d8dee8c60b9e3d7a24
Eric Zorn <goldencatch93@gmail.com>
2017-09-17 15:50:30 -0400

    First commit

---
+++ Module 2/Demo/Example.java
@@ -0,0 +1,18 @@
+//This is an example class which will demo displaying output from a Java program displaying output from a Java program
+public class Example {
+
+   //This is the main method
+	public static void main (String[] args) {
+      //This is an output statement
+		
+		System.out.println("Hello World!");
+		System.out.println ("This is line 1");
+		System.out.println ("This is line 2");
+		
+		System.out.print ("First\t\tLast\n");
+		System.out.println ("Joe\tDoe");
+      
+		System.out.print("My age is: ");
+		System.out.println(14);
+	}
+}
---
+++ javaTest/helloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window along with whatever is in the strings I added.
+        String hello = "Hello World, my Name is Eric Zorn";
+
+        int age = 21;
+        
+        boolean ageTrue = true;
+
+        System.out.println(hello + " and I am " + age + " years old! And....this is vey " + ageTrue + "!");
+    }
+
+}
